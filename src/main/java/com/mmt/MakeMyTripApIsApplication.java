2dfd4e9a50b566c67d3858d9fc548dd20cd1a39a
Gaurav Bansal <Gaurav.Bansal@igglobal.com>
2018-08-06 15:37:36 +0530

    initial commit

---
+++ src/main/java/com/mmt/MakeMyTripApIsApplication.java
@@ -0,0 +1,12 @@
+package com.mmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyTripApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeMyTripApIsApplication.class, args);
+	}
+}
