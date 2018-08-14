d3ca53908870179d86df8f7543498d1752eab976
Gaurav Bansal <Gaurav.Bansal@igglobal.com>
2018-08-14 12:03:49 +0530

    Blog controller

--- src/main/java/com/mmt/controller/BlogController.java
+++ src/main/java/com/mmt/controller/BlogController.java
@@ -3,6 +3,7 @@ package com.mmt.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,28 +16,28 @@ import com.mmt.service.BlogServiceImpl;
 @RestController("travelBlog")
 @RequestMapping("/mmt/blog")
 public class BlogController {
-	
+
 	@Autowired
 	private BlogServiceImpl blogImpl;
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
-	@RequestMapping(method = RequestMethod.POST, consumes= {MediaType.APPLICATION_JSON_VALUE})
+	@RequestMapping(value = "/writeNew", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
 	public void addBlog(@RequestBody TravelBlogsDto tBlogDto) {
 		blogImpl.addNewBlogs(tBlogDto);
 	}
-	
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseData showBlog(long id) {
+
+	@RequestMapping(value = "/open_blog/{id}", method = RequestMethod.GET)
+	public ResponseData showBlog(@PathVariable(value = "id") long id) {
 		TravelBlogsDto blogData;
 		blogData = blogImpl.readBlogs(id);
-		ResponseData blogResponse = new ResponseData(null, "Opening Blog", blogData, null);
+		ResponseData blogResponse = new ResponseData("200", "Opening Blog", blogData, null);
 		return blogResponse;
 	}
-	
+
 	@PreAuthorize("hasRole('ADMIN')")
-	@RequestMapping(method = RequestMethod.DELETE, consumes= {MediaType.APPLICATION_JSON_VALUE})
-	public void deleteBlog(long id) {
+	@RequestMapping(value = "/moveToTrash/{id}", method = RequestMethod.DELETE)
+	public void deleteBlog(@PathVariable(value = "id") long id) {
 		blogImpl.removeBlogs(id);
 	}
-	
+
 }
